8d77ba8cb42a3042f1e823d26687d0ccb43c7cd3
MinToan35 <mintoan351998@gmail.com>
2022-09-25 10:34:48 +0700

    fix

--- src/context/constants.js
+++ src/context/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://certificate-sheet-server.herokuapp.com/api";
+    : "https://certificate-sheet-app.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "token";
 
